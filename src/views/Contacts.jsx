46606589d1e8c1f518daf52517a6bf0fb4135c4f
jon.roji <jon.roji@sngular.com>
2023-05-15 19:16:58 +0200

    list-filters

--- src/views/Contacts.jsx
+++ src/views/Contacts.jsx
@@ -3,6 +3,7 @@ import Contact from "../components/Contact/Contact";
 
 function Contacts() {
     let [data, setData] = useState([]);
+    let [currentFilter, setCurrentFilter] = useState('regular');
     // let data = [];
     // useEffect, useState
     useEffect(() => {
@@ -15,7 +16,16 @@ function Contacts() {
 
     return <div>
         <h1>Lista de contactos</h1>
-        {data.map(item => <Contact extraClass={item.featured ? 'xl' : 'l'} img={item.img} name={item.name} icon={item.messages[0]}/>)}
+        <button onClick={() => setCurrentFilter('regular')}>Regular users</button>
+        <button onClick={() => setCurrentFilter('premium')}>Premium users</button>
+
+        {data.map(item => {
+            if ((currentFilter === 'regular' && item.premium === false) || (currentFilter === 'premium' && item.premium === true)) {
+                return <Contact extraClass={item.premium ? 'background-red' : ''} img={item.img} name={item.name} icon={item.messages[0]}/>
+            } else {
+                return ''
+            }
+        })}
     </div>
 }
 
